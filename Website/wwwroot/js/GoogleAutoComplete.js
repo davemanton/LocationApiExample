b88a93fc46519190792df152b17c428987da5dfb
DaveManton <d_e_manton@hotmail.com>
2017-06-28 22:23:30 +0100

    Search results page created

---
+++ Website/wwwroot/js/GoogleAutoComplete.js
@@ -0,0 +1,11 @@
+﻿var options = {
+	componentRestrictions: {
+		country: 'gb'
+	}
+}
+
+var originInput = document.getElementById("origin-input");
+var autocomplete = new google.maps.places.Autocomplete(originInput, options);
+
+var destinationInput = document.getElementById("destination-input");
+var autocomplete = new google.maps.places.Autocomplete(destinationInput, options);
---
+++ Website/wwwroot/js/GoogleMapsBackground.js
@@ -0,0 +1,17 @@
+﻿if (navigator.geolocation) {
+	navigator.geolocation.getCurrentPosition(updateMap);
+} else {
+	updateMap();
+}
+
+function updateMap(position) {
+
+	var latlon = position != null ? position.coords.latitude + "," + position.coords.longitude : "53.480506,-2.23687";
+
+	var imgUrl = "https://maps.googleapis.com/maps/api/staticmap?center=" + latlon + "&zoom=14&size=1920x1080&sensor=false&key=AIzaSyD2lln26f5wLuV-SEGaIG6G2kRykEQ5gEU";
+
+	$(document).ready(function () {
+		$('#background').css('background-image', "url('" + imgUrl + "')");
+		$('#background').css('background-size', "cover");
+	});
+}
